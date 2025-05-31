917a5152879eca5aeec511b7c9e34d4cbd360c87
GRosvalds <gabriels.rosvalds@gmail.com>
2025-05-31 22:44:32 +0300

    made that the seeder only runs if there is no data in the tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,21 +7,38 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            RoleSeeder::class,
-            UserSeeder::class,
-            PermissionSeeder::class,
-            RoleUserTableSeeder::class,
-            InventoryItemsTableSeeder::class,
-            ItemUserTableSeeder::class,
-            RequestStatusesTableSeeder::class,
-            LeaseRequestsTableSeeder::class,
-            LeaseExtensionRequestsTableSeeder::class,
-        ]);
+        if (DB::table('roles')->count() === 0) {
+            $this->call(RoleSeeder::class);
+        }
+        if (DB::table('users')->count() === 0) {
+            $this->call(UserSeeder::class);
+        }
+        if (DB::table('permissions')->count() === 0) {
+            $this->call(PermissionSeeder::class);
+        }
+        if (DB::table('role_user')->count() === 0) {
+            $this->call(RoleUserTableSeeder::class);
+        }
+        if (DB::table('inventory_items')->count() === 0) {
+            $this->call(InventoryItemsTableSeeder::class);
+        }
+        if (DB::table('item_user')->count() === 0) {
+            $this->call(ItemUserTableSeeder::class);
+        }
+        if (DB::table('request_statuses')->count() === 0) {
+            $this->call(RequestStatusesTableSeeder::class);
+        }
+        if (DB::table('lease_requests')->count() === 0) {
+            $this->call(LeaseRequestsTableSeeder::class);
+        }
+        if (DB::table('lease_extension_requests')->count() === 0) {
+            $this->call(LeaseExtensionRequestsTableSeeder::class);
+        }
     }
 }
